ca3090844f25e61b2d5e254b401a33b1fa825f10
emilia-adamska <emsheerio12@gmail.com>
2021-05-01 22:09:21 +0200

    addes plural files, added new random numbers generator

---
+++ ajax.js
@@ -0,0 +1,32 @@
+var firstPart, middlePart, endPart, firstPartPlural, middlePartPlural;
+$.ajax({
+    type: 'GET',
+    async: false,
+    url: "txtfiles/WHO.txt",
+    success: function(data) { firstPart = data.split(/\r?\n/);}
+  });
+  $.ajax({
+    type: 'GET',
+    async: false,
+    url: "txtfiles/WHAT.txt",
+    success: function(data) { middlePart = data.split(/\r?\n/);}
+  });
+  $.ajax({
+    type: 'GET',
+    async: false,
+    url: "txtfiles/WHOPLURAL.txt",
+    success: function(data) { firstPartPlural = data.split(/\r?\n/);}
+  });
+  $.ajax({
+    type: 'GET',
+    async: false,
+    url: "txtfiles/WHATPLURAL.txt",
+    success: function(data) { middlePartPlural = data.split(/\r?\n/);}
+  });
+  $.ajax({
+    type: 'GET',
+    async: false,
+    url: "txtfiles/ENDOFSENTENCE.txt",
+    success: function(data) { endPart = data.split(/\r?\n/);}
+  });
+ var numberOfParts = [firstPart.length, middlePart.length, endPart.length, firstPartPlural.length, middlePartPlural.length];
--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-'use strict';
-
-/*var texts = $.get('who.txt', function(response) {  //line 2
-    var outerScopeVar = "123";
-},"text");
-console.log(texts);
-*/
-var firstPart, middlePart, endPart;
-$.ajax({
-    type: 'GET',
-    async: false,
-    url: "WHO.txt",
-    success: function(data) { firstPart = data.split(/\r?\n/);}
-  });
-  $.ajax({
-    type: 'GET',
-    async: false,
-    url: "WHAT.txt",
-    success: function(data) { middlePart = data.split(/\r?\n/);}
-  });
-  $.ajax({
-    type: 'GET',
-    async: false,
-    url: "ENDOFSENTENCE.txt",
-    success: function(data) { endPart = data.split(/\r?\n/);}
-  });
-  firstPart.length;
+  var randomNumbers = new Array();
   function generateSentence(){
-    let randomNumbers = [Math.floor(Math.random() * 50),Math.floor(Math.random() * 50),Math.floor(Math.random() * 50)];
-    document.getElementById("sentence").innerHTML = firstPart[randomNumbers[0]] + " " + middlePart[randomNumbers[1]] + " " + endPart[randomNumbers[2]];
+      for( var i = 0; i < numberOfParts.length; i++){
+       randomNumbers[i] = Math.floor(Math.random() * numberOfParts[i])
+}
+    //[Math.floor(Math.random() * numberOfParts[0]),Math.floor(Math.random() * numberOfParts[0]),Math.floor(Math.random() * 50)];
+    //document.getElementById("sentence").innerHTML = firstPart[randomNumbers[0]] + " " + middlePart[randomNumbers[1]] + " " + endPart[randomNumbers[2]];
 }
 document.getElementById("generate").addEventListener("click",generateSentence);
