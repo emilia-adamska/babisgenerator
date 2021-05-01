e7784f24bef3d0caf49ff2f9630f88097f8c3c27
emilia-adamska <emsheerio12@gmail.com>
2021-05-01 21:27:04 +0200

    script fetching data from files

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 'use strict';
-var firstPart = ["My dog", "Minister of health", "Minister of Agriculture"];
-var middlePart = ["jumped on the fence", "ordered 69 vaccines", "banned farming simulator"];
-var lastPart = ["because he liked to eat.", "because he refused to order Sputnik vaccine.", "to send farmers to fields."];
-function generateSentence(){
-    let randomNumbers = [Math.floor(Math.random() * 2),Math.floor(Math.random() * 2),Math.floor(Math.random() * 2)];
-    document.getElementById("sentence").innerHTML = firstPart[randomNumbers[0]] + " " + middlePart[randomNumbers[1]] + " " + lastPart[randomNumbers[2]];
-    
+
+/*var texts = $.get('who.txt', function(response) {  //line 2
+    var outerScopeVar = "123";
+},"text");
+console.log(texts);
+*/
+var firstPart, middlePart, endPart;
+$.ajax({
+    type: 'GET',
+    async: false,
+    url: "WHO.txt",
+    success: function(data) { firstPart = data.split(/\r?\n/);}
+  });
+  $.ajax({
+    type: 'GET',
+    async: false,
+    url: "WHAT.txt",
+    success: function(data) { middlePart = data.split(/\r?\n/);}
+  });
+  $.ajax({
+    type: 'GET',
+    async: false,
+    url: "ENDOFSENTENCE.txt",
+    success: function(data) { endPart = data.split(/\r?\n/);}
+  });
+  firstPart.length;
+  function generateSentence(){
+    let randomNumbers = [Math.floor(Math.random() * 50),Math.floor(Math.random() * 50),Math.floor(Math.random() * 50)];
+    document.getElementById("sentence").innerHTML = firstPart[randomNumbers[0]] + " " + middlePart[randomNumbers[1]] + " " + endPart[randomNumbers[2]];
 }
 document.getElementById("generate").addEventListener("click",generateSentence);
-//#db1f2d
